77e7de148e5896bc60ca55a81d1026528b9c4f87
Joao MC Teixeira <joaomcteixeira@gmail.com>
2019-10-08 11:54:45 -0400

    Bump version: 0.0.8 → 0.0.9

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -28,7 +28,7 @@ project = 'SampleProject'
 year = '2019'
 author = 'Joao MC Teixeira'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.8'
+version = release = '0.0.9'
 
 pygments_style = 'trac'
 templates_path = ['.']
