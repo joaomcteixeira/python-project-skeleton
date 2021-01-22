c3540fca06363973d86f359e272dbfb091f79672
joaomcteixeira <joaomcteixeira@users.noreply.github.com>
2021-01-22 02:33:24 +0000

    [SKIP] version bump 0.2.2 -> 0.3.0
    [FEATURE] Pull Request #9
    
    Completes Missings in CI details.
    See changelog.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ long_description = '{}\n{}'.format(
 
 setup(
     name='jmct-sampleproject',
-    version='0.2.2',
+    version='0.3.0',
     description='A skeleton template for Python projects.',
     long_description=long_description,
     author='Joao Miguel Correia Teixeira',
